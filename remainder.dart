c3abb2cfefd68a9c6c5d165177f20492d367c75e
Narongrit3125 <you@example.com>
2024-11-18 15:21:46 +0700

    finish work

---
+++ remainder.dart
@@ -0,0 +1,11 @@
+void main(){
+  var num1 = 10;
+  var num2 = 3;
+  int mod = num1 % num2;
+  double div = num1 / num2;
+
+  print("output");
+  print("quotient = ${div.toStringAsFixed(0)}");
+  print("remainder= $mod");
+
+}
