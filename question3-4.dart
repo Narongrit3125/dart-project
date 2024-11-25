80d3ca00f84e8440f011785502637c55f4f7d23a
Narongrit3125 <nbplaying4343@gmail.com>
2024-11-25 15:26:15 +0700

    new Q2-3

--- question3-4.dart
+++ question3-4.dart
@@ -1,19 +1,13 @@
-import 'dart:io';
-
 double calculateArea({double length = 1, double width = 1}) {
   return length * width;
 }
 
 void main() {
-  // รับค่า length จากผู้ใช้
-  print("Enter the length:");
-  double length = double.parse(stdin.readLineSync()!);
+  // คำนวณพื้นที่ด้วยค่าเริ่มต้นของ length และ width
+  double area1 = calculateArea();
+  print('Area with default values: $area1');
 
-  // รับค่า width จากผู้ใช้
-  print("Enter the width:");
-  double width = double.parse(stdin.readLineSync()!);
-
-  // คำนวณพื้นที่
-  double area = calculateArea(length: length, width: width);
-  print('The area of the rectangle is: $area');
+  // คำนวณพื้นที่ด้วยการระบุค่า length และ width
+  double area2 = calculateArea(length: 5, width: 10);
+  print('Area with length 5 and width 10: $area2');
 }
